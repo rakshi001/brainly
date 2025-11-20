8bb2f6523ff26e92d395d3538abd7fd67cf2b731
Rakshith M L <rakshithml078@gmail.com>
2025-11-21 00:35:23 +0530

    delete end point

--- src/index.ts
+++ src/index.ts
@@ -86,9 +86,38 @@ app.post('/api/v1/content', userMiddleware, async (req, res) => {
   });
 });
 
-app.get('/api/v1/content', (req, res) => {});
+app.get('/api/v1/content', userMiddleware, async (req, res) => {
+  const userId = (req as any).userId;
+  const content = await ContentModel.find({
+    userId: userId,
+  }).populate('userId', 'username');
+  res.json({
+    content,
+  });
+});
 
-app.delete('/api/v1/content', (req, res) => {});
+app.delete('/api/v1/content', async (req, res) => {
+  const contentId = req.body.contentId;
+  const userId = (req as any).userId;
+
+  try {
+    const deleteContent = await ContentModel.deleteMany({
+      contentId,
+      userId: userId,
+    });
+    if (deleteContent) {
+      res.json({
+        message: 'content got deleted',
+      });
+    } else {
+      res.send('error');
+    }
+  } catch (e) {
+    res.status(401).json({
+      message: { e },
+    });
+  }
+});
 
 app.post('/api/v1/brain/share', (req, res) => {});
 
